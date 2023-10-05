9be5ccfc06793fe153668dd29da3fd273b19e7e9
WenJenChun <daydreamsss@hotmail.com>
2023-10-05 23:10:37 +0100

    add pages

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
+import './assets/scss/all.scss';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
