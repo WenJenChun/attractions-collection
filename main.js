b8eb6e145ee9c8eb954c05f2c934e9287037d55d
WenJenChun <daydreamsss@hotmail.com>
2023-10-07 11:53:18 +0100

    seperate index.js from main.js

--- main.js
+++ main.js
@@ -4,35 +4,4 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 // import { getAttractionDetail } from './detail';
 
 
-const attraction = document.querySelector('#attraction');
-const _url="http://localhost:3000";
-
-//獲取 api 資料並顯示在網頁上
-function init(){
-  axios.get(_url+"/views")
-  .then(function(response){
-    console.log(response.data);
-    const apiDatas = response.data;
-    let str = "";
-    apiDatas.forEach(function(item){
-      str += `
-          <div class="col">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title text-center">${item.name}</h5>
-                <p class="card-text">${item.description.slice(0,45)}...</p>
-                <div class="text-center">
-                  <a href="./detail.html?id=${item.id}" class="btn btn-secondary text-white mt-2">看看</a>
-                  <a href="#" class="btn btn-secondary text-white mt-2">收藏</a>
-                </div>
-              </div>
-            </div>
-          </div>`;
-    });
-
-    attraction.innerHTML = str;
-  });
-};
-
-init();
 
