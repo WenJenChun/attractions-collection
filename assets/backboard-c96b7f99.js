183fbc52bc644fd2db55b86ae62154c0e1a2f82d
WenJenChun <daydreamsss@hotmail.com>
2023-10-22 00:14:55 +0100

    Updates

---
+++ assets/backboard-c96b7f99.js
@@ -0,0 +1,17 @@
+import"./main-be82571a.js";const r=document.querySelector("#attractionList"),c="https://attractions-api-jhwt.onrender.com",l=document.querySelector("#navBar");function s(){axios.get(c+"/views").then(function(e){console.log(e.data);const i=e.data;let o="";i.forEach(function(t){o+=`
+        <tr>
+            <th scope="row">${t.id}</th>
+            <td>${t.name}</td>
+            <td>${t.description}</td>
+            <td>
+              <a href="/edit.html?id=${t.id}" class="btn btn-sm btn-secondary text-white">編輯</a>
+              <input data-attraction-id=${t.id} type="button" class="mt-1 deleteAttraction btn btn-sm btn-warning text-white" value="刪除">
+            </td>
+        </tr>`}),r.innerHTML=o,document.querySelectorAll(".deleteAttraction").forEach(function(t){t.addEventListener("click",function(){const n=t.getAttribute("data-attraction-id");console.log("按"+n),axios.delete(`${c}/views/${n}`).then(function(a){alert("刪除成功！"),location.reload()}).catch(function(a){console.error("刪除失敗："+a)})})})})}s();localStorage.getItem("token")==null?console.log("還沒登入"):(console.log("已登入"),localStorage.getItem("role")=="admin"?l.innerHTML=` 
+    <a class="me-3" href="/backboard.html">後台</a>
+    <a class="me-3" href="/collections.html">我的收藏</a>
+    <input id="logoutBtn" type="button" class="btn btn-secondary text-white " value="登出">
+    `:l.innerHTML=` 
+    <a class="me-3" href="/collections.html">我的收藏</a>
+    <input id="logoutBtn" type="button" class="btn btn-secondary text-white " value="登出">
+    `,document.querySelector("#logoutBtn").addEventListener("click",function(){localStorage.removeItem("token"),localStorage.removeItem("role"),localStorage.removeItem("userId"),location.reload(),window.location.href="https://wenjenchun.github.io/attractions-collection/index.html"}));
--- assets/collections-1b7e3f3b.js
+++ assets/collections-1b7e3f3b.js
@@ -1,4 +1,4 @@
-import"./main-5aeeef5c.js";const n=document.querySelector("#collections"),c="https://attractions-api-jhwt.onrender.com",l=document.querySelector("#navBar");let s=localStorage.getItem("userId");function i(){axios.get(c+"/collections?userId="+s+"&_expand=view").then(function(t){console.log(t.data);const a=t.data;let o="";a.forEach(function(e){o+=`
+import"./main-be82571a.js";const a=document.querySelector("#collections"),c="https://attractions-api-jhwt.onrender.com",l=document.querySelector("#navBar");let i=localStorage.getItem("userId");function s(){axios.get(c+"/collections?userId="+i+"&_expand=view").then(function(t){console.log(t.data);const n=t.data;let o="";n.forEach(function(e){o+=`
           <div class="col">
             <div class="card">
               <div class="card-body">
@@ -9,11 +9,11 @@ import"./main-5aeeef5c.js";const n=document.querySelector("#collections"),c="htt
                 </div>
               </div>
             </div>
-          </div>`}),n.innerHTML=o})}i();localStorage.getItem("token")==null?console.log("還沒登入"):(console.log("已登入"),localStorage.getItem("role")=="admin"?l.innerHTML=` 
+          </div>`}),a.innerHTML=o})}s();localStorage.getItem("token")==null?console.log("還沒登入"):(console.log("已登入"),localStorage.getItem("role")=="admin"?l.innerHTML=` 
     <a class="me-3" href="/pages/backboard.html">後台</a>
     <a class="me-3" href="/pages/collections.html">我的收藏</a>
     <input id="logoutBtn" type="button" class="btn btn-secondary text-white " value="登出">
     `:l.innerHTML=` 
     <a class="me-3" href="/pages/collections.html">我的收藏</a>
     <input id="logoutBtn" type="button" class="btn btn-secondary text-white " value="登出">
-    `,document.querySelector("#logoutBtn").addEventListener("click",function(){localStorage.removeItem("token"),localStorage.removeItem("role"),localStorage.removeItem("userId"),location.reload(),window.location.href="/index.html"}));
+    `,document.querySelector("#logoutBtn").addEventListener("click",function(){localStorage.removeItem("token"),localStorage.removeItem("role"),localStorage.removeItem("userId"),location.reload(),window.location.href="https://wenjenchun.github.io/attractions-collection/index.html"}));
