cc1813a3a0367558720017bc6b2933d7ddb8a2e6
WenJenChun <daydreamsss@hotmail.com>
2023-10-23 20:31:16 +0100

    re write logic for detail.js

--- detail.js
+++ detail.js
@@ -3,7 +3,7 @@ const isAdmin = localStorage.getItem("role") === "admin";
 const userId = localStorage.getItem("userId");
 
 const navBar = document.querySelector('#navBar');
-
+let viewInfo;
 const placeName = document.querySelector('#placeName');
 const placeDetail = document.querySelector('#placeDetail');
 const viewId = location.href.split("=")[1];
@@ -17,33 +17,63 @@ let collectId;
 const _url="https://attractions-api-jhwt.onrender.com";
 
 
-//獲取用戶收藏資訊
-function getUserCollection() {
-  return axios.get(_url + "/collections?userId=" + userId)
-    .then(function (response) {
-      userCollects = response.data;
-    });
-}
+//1. 獲取景點資訊
+//2. 判斷用戶是否登入
+//   已登入：抓取用戶已收藏資料 > 比對 > btn 文字&navBar 
+//   未登入：btn 文字 & navBar
 
-//獲取用戶收藏資訊後，獲取景點詳細資訊
-//兩者比對資訊判斷按鈕文字是已收藏或尚未收藏
-async function getAttractionDetail() {
-  await getUserCollection();
-  console.log(userCollects);
-
-
+function getViewInfo(){
   axios.get(_url+"/views/"+viewId)
   .then(function(response){
     const apiData = response.data;
     placeName.textContent = apiData.name;
     placeDetail.textContent = apiData.description;
+    viewInfo = apiData;
+  });
+}
 
-    //比對 viewId 是否在 userCollects (箭頭寫法&完整寫法)
-    const isCollected = userCollects.find(obj => obj.viewId === viewId); 
-    // const isCollected = userCollects.find(function(obj) {
-    //   return obj.viewId === viewId;
-    // });
+getViewInfo();
 
+async function checkLogIn(){
+  if(!isLogIn){
+    navBar.innerHTML =
+      ` 
+      <a class="me-3" href="/attractions-collection/login.html">登入</a>
+      <a class="me-3" href="/attractions-collection/register.html">註冊</a>
+      `;
+      collectBtn.addEventListener("click", function(){
+        alert("請先登入!");
+        window.location.href = "https://wenjenchun.github.io/attractions-collection/login.html";
+      });
+      collectBtn.textContent = "尚未收藏";
+  } else {
+    if(isAdmin){
+      navBar.innerHTML =
+      ` 
+      <a class="me-3" href="/attractions-collection/backboard.html">後台</a>
+      <a class="me-3" href="/attractions-collection/collections.html">我的收藏</a>
+      <input id="logoutBtn" type="button" class="btn btn-secondary text-white " value="登出">
+      `;
+    } else {
+      navBar.innerHTML =
+      ` 
+      <a class="me-3" href="/attractions-collection/collections.html">我的收藏</a>
+      <input id="logoutBtn" type="button" class="btn btn-secondary text-white " value="登出">
+      `;
+    }
+    const logoutBtn = document.querySelector('#logoutBtn');
+    logoutBtn.addEventListener("click", function(){
+      localStorage.removeItem("token");
+      localStorage.removeItem("role");
+      localStorage.removeItem("userId");
+      location.reload();
+      window.location.href = "https://wenjenchun.github.io/attractions-collection/index.html";
+    });
+
+    //抓取用戶已收藏資料
+    await getUserCollection();
+    //比對 
+    const isCollected = userCollects.find(viewInfo => viewInfo.viewId === viewId);
     if(isCollected){
       //取該景點在 userCollects 的 id 
       collectId=isCollected.id;
@@ -54,69 +84,20 @@ async function getAttractionDetail() {
       console.log('還沒收');
     }
 
-  });
+
+  }
 }
 
-getAttractionDetail();
+checkLogIn();
 
-collectBtn.addEventListener("click", function(){
-  if(localStorage.getItem("token")==null){
-    alert("請先登入!");
-    window.location.href = "https://wenjenchun.github.io/attractions-collection/login.html";
-  }
 
-  if(collectBtn.textContent =="尚未收藏"){
-    collectBtn.textContent ="已收藏!"
-    axios.post(_url+"/collections",{
-      "viewId": viewId,
-      "userId": localStorage.getItem("userId"),
-  }).then(function(res){
-      console.log("response 回傳");
-      console.log(res.data);
-  }).catch(function(error){
-      console.log("錯誤訊息");
-      console.log(error.response)
-  });
-  } else {
-    //這邊是已經收藏要改為「尚未收藏」
-    collectBtn.textContent = "尚未收藏";
-    axios.delete(`${_url}/collections/${collectId}`)
-    .then(function(res){
-      console.log("response 回傳");
-      console.log(res.data);
-  }).catch(function(error){
-      console.log("錯誤訊息");
-      console.log(error.response)
-  });
+//////////////////////////////////////////////////////////////
 
-  }
-});
-
-if(localStorage.getItem("token")==null){
-  console.log('還沒登入');
-} else {
-  console.log('已登入');
-  
-  if(localStorage.getItem("role")=="admin"){
-    navBar.innerHTML =
-    ` 
-    <a class="me-3" href="/backboard.html">後台</a>
-    <a class="me-3" href="/collections.html">我的收藏</a>
-    <input id="logoutBtn" type="button" class="btn btn-secondary text-white " value="登出">
-    `;
-  } else {
-    navBar.innerHTML =
-    ` 
-    <a class="me-3" href="/collections.html">我的收藏</a>
-    <input id="logoutBtn" type="button" class="btn btn-secondary text-white " value="登出">
-    `;
-  }
-  const logoutBtn = document.querySelector('#logoutBtn');
-  logoutBtn.addEventListener("click", function(){
-    localStorage.removeItem("token");
-    localStorage.removeItem("role");
-    localStorage.removeItem("userId");
-    location.reload();
-    window.location.href = "https://wenjenchun.github.io/attractions-collection/index.html";
-  });
+//獲取用戶收藏資訊
+function getUserCollection() {
+  return axios.get(_url + "/collections?userId=" + userId)
+    .then(function (response) {
+      userCollects = response.data;
+    });
 }
+
